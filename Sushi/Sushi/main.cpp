cecb3c9df362e46c5f5c9dbd942b1caacbb3efec
Danny <noahark0628@gmail.com>
2017-02-15 22:22:24 +0900

    Add sort, temporary test
    -wrong solution

--- Sushi/Sushi/main.cpp
+++ Sushi/Sushi/main.cpp
@@ -1,38 +1,64 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 #define MAX_LIST 20
 
-struct Dish {
-	int price;
-	int prior;
-	float avg;
-	Dish() {
-	};
-	void avrage() {
-		avg = (float)price / prior;
-	}
-	void print() {
-		printf("Price: %d\tPrior: %d\tAvg: %f.2\n", price, prior, avg);
-	}
+using namespace std;
+
+class Dish {
+public:
+		int price;
+		int pref;
+		float avg;
+		Dish(int price, int pref) {
+			this->price = price;
+			this->pref = pref;
+			avrage();
+		};
+		void avrage() {
+			avg = (float)price / pref;
+		}
+		void print() {
+			cout << "Price: " << price << "\tPrior: "<< pref << "\tAvg: " << avg <<endl;
+		}
 };
+bool operator < (const Dish &a, const Dish &b) {
+	return a.avg < b.avg;
+}
 
 int main() {
-	int Case, n, m, i, j, sort[MAX_LIST];
-	Dish list[MAX_LIST];
-	Dish tmp;
+	int Case, n, m, Price, Pref, min, result = 0, sum=0;
+	vector <Dish> list;
+	//Dish tmp(0, 0);
 
 	// input Num.Test Case
-	scanf("%d", &Case);
-	// input Num.people, Budget
-	scanf("%d %d", &n, &m);
+	cin >> Case;
+	// input Num.menu types, Budget
+	cin >> n >> m;
 	// input Data
-	i = 0;
-	while (n--) {
-		scanf("%d %d", &tmp.price, &tmp.prior);
-		tmp.avrage();
-		list[i] = tmp;
-		i++;
+	//M_avg = 0;
+	for (int i = 0; i < n; i++) {
+		cin>> Price >> Pref;
+		list.push_back(Dish(Price, Pref));
 	}
-
+	sort(list.begin(), list.end());
+	min = m;
+	for (int i = 0; i < n; i++) {
+		if (list[i].price <= min) min = list[i].price;
+	}
+		
+	while (1) {
+		for (int i = 0; i < n; i++) {
+			if (sum + list[i].price < m) {
+				sum += list[i].price;
+				result += list[i].pref;
+				cout << result << endl;
+				break;
+			}
+		}
+		if (sum + min > m) break;
+	};
+	cout << result << endl;
 	scanf("%d", &Case);
 }
