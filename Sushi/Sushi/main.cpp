3eb462375b025f89ad3d642ce0e43d2d231e04ac
Danny <noahark0628@gmail.com>
2017-02-20 16:56:39 +0900

    Revert "wdwwqd"
    This reverts commit 36845f7e1a48ff07667f4c28bdcc072b2a618a3b.

--- Sushi/Sushi/main.cpp
+++ Sushi/Sushi/main.cpp
@@ -1,41 +1,64 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <vector>
 #include <algorithm>
-#include <string.h>
 #define MAX_LIST 20
-#define TABLE_SIZE 201
 
 using namespace std;
 
+class Dish {
+public:
+		int price;
+		int pref;
+		float avg;
+		Dish(int price, int pref) {
+			this->price = price;
+			this->pref = pref;
+			avrage();
+		};
+		void avrage() {
+			avg = (float)price / pref;
+		}
+		void print() {
+			cout << "Price: " << price << "\tPrior: "<< pref << "\tAvg: " << avg <<endl;
+		}
+};
+bool operator < (const Dish &a, const Dish &b) {
+	return a.avg < b.avg;
+}
+
 int main() {
-	int Case;
-	unsigned int knap[TABLE_SIZE];
+	int Case, n, m, Price, Pref, min, result = 0, sum=0;
+	vector <Dish> list;
+	//Dish tmp(0, 0);
+
 	// input Num.Test Case
 	cin >> Case;
-	while (Case--) {
-		memset(knap, 0, sizeof(knap));
-		int N;
-		long long B;
-		long long price[MAX_LIST];
-		unsigned int pref[MAX_LIST];
-		// input Num.menu types, Budget
-		cin >> N >> B;
-		B /= 100;
-		// input Data
-		for (int i = 0; i < N; i++) {
-			cin >> price[i] >> pref[i];
-			price[i] /= 100;
-		}
+	// input Num.menu types, Budget
+	cin >> n >> m;
+	// input Data
+	//M_avg = 0;
+	for (int i = 0; i < n; i++) {
+		cin>> Price >> Pref;
+		list.push_back(Dish(Price, Pref));
+	}
+	sort(list.begin(), list.end());
+	min = m;
+	for (int i = 0; i < n; i++) {
+		if (list[i].price <= min) min = list[i].price;
+	}
 		
-		for (int p = 1; p <= B; p++) {
-			unsigned int& x = knap[p % TABLE_SIZE];
-			x = 0;
-			for (int i = 0; i < N; i++) {
-				if (p >= price[i])
-					x = max<unsigned int>(x, knap[(p - price[i]) % TABLE_SIZE] + pref[i]);
+	while (1) {
+		for (int i = 0; i < n; i++) {
+			if (sum + list[i].price < m) {
+				sum += list[i].price;
+				result += list[i].pref;
+				cout << result << endl;
+				break;
 			}
 		}
-		
-		cout << knap[B % TABLE_SIZE] << endl;
-	}
-	return 0;
+		if (sum + min > m) break;
+	};
+	cout << result << endl;
+	scanf("%d", &Case);
 }
