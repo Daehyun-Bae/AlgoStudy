c3518afcc5f865dacf5c5fd69a176d3378510f41
Danny <noahark0628@gmail.com>
2020-12-30 02:31:50 +0900

    Create pgr_h-index.py

---
+++ Programmers/pgr_h-index.py
@@ -0,0 +1,20 @@
+'''
+ex
+citations	    return
+[3, 0, 6, 1, 5] 3
+'''
+
+def solution(citations):
+    citations = sorted(citations, reverse=True)
+    answer = len([i+1 for i in range(len(citations)) if i+1 <= citations[i]])
+
+    return answer
+
+cases = [[3, 0, 6, 1, 5],   # 3
+         [0, 0, 0, 0, 0],   # 0
+         [25, 8, 5, 3, 3],  # 3
+         [10, 8, 5, 4, 3]  # 4
+         ]
+
+for case in cases:
+    print(solution(case))
