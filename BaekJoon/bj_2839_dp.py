b2f3c208f6fe84f3f9f9c71e40eb615db4868b49
Daehyun-bae <noahark0628@gmail.com>
2021-01-06 21:00:42 +0900

    Update bj_2839_dp.py

--- BaekJoon/bj_2839_dp.py
+++ BaekJoon/bj_2839_dp.py
@@ -5,9 +5,8 @@ from itertools import combinations, permutations, product
 
 
 def solution(N):
-    # memo = {1: [3, 5]}  # Initialize
     memo = {3: 1, 5: 1}  # Initialize
-    i = 2
+
     print(f'N: {N}')
     if N in memo:
         return memo[N]
@@ -27,29 +26,6 @@ def solution(N):
 
         if not results:
             return -1
-
-        #
-        # results = []
-        # for j in range(1, i//2 + 1):
-        #     print(j)
-        #     # arr1 = memo[j]
-        #     arr1 = filter(lambda x: memo[x] == j, memo.keys())
-        #     # arr2 = memo[i - j]
-        #     arr2 = filter(lambda x: memo[x] == i - j, memo.keys())
-        #     sub_result = set(map(lambda x: x[0] + x[1], product(arr1, arr2)))
-        #     sub_result = list(filter(lambda x: x <= N, sub_result))
-        #     if N in sub_result:
-        #         return i
-        #     for k in sub_result:
-        #         if k not in memo:
-        #             memo[k] = i
-        #     results += sub_result
-        # if not results:
-        #     return -1
-        #
-        # # memo[i] = results
-        # i += 1
-
 
 # N = int(sys.stdin.readline())
 
